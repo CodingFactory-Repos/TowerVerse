0f25233f2d23519d7a1f205499c3686ef9af656f
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2023-11-17 15:38:56 +0100

    Add more to map

--- Assets/Script/OpenDoor.cs
+++ Assets/Script/OpenDoor.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OpenDoor : MonoBehaviour
+public class OpenDoor : MonoBehaviour, IInteractable
 {
     public GameObject door;
 
-    // Start is called before the first frame update
-    void Start()
+    public string GetDescription()
+    {
+        return "Open Door";
+    }
+
+    public void Interact()
     {
         // Turn door 90 degrees on the y axis
-        door.transform.Rotate(0, 90, 0);
+        door.transform.Rotate(0, -90, 0);
 
-        // Move door to the right
-        door.transform.position = new Vector3(door.transform.position.x + 1.5f, door.transform.position.y, door.transform.position.z);
+        // Move door to the left
+        door.transform.position = new Vector3(door.transform.position.x - 1.5f, door.transform.position.y, door.transform.position.z);
     }
 }
